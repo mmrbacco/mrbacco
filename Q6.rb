c8376a5b488fa33471fdd87e589e52713f79360e
mrbacco <abaccolini@gmail.com>
2021-01-21 19:14:32 +0000

    Update Q6.rb

--- Q6.rb
+++ Q6.rb
@@ -2,25 +2,20 @@
 
 # define Singer class and it's own method line1,2,3 and 4 with their outputs
 class Singer
-  
   def line1
-    puts "cause the players gonna #{"play! " * 5}" #taking the output from Q5
-    
+    puts "cause the players gonna #{"play! " * 5}" #taking the output from Q
   end
 
   def line2
     puts "I shake it off, I shake it off"
-    
   end
 
   def line3
     puts "Heartbreakers gonna #{"break! " * 5}"
-    
   end
 
   def line4
-    puts "And the fakers gonna #{"fake! " * 5}"
-    
+    puts "And the fakers gonna #{"fake! " * 5}
   end
 
 end
