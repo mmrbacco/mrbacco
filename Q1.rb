a0fc7f23520ef3ef1ec42b947f8cf94985561d34
mrbacco <abaccolini@gmail.com>
2021-01-21 19:10:35 +0000

    Update Q1.rb

--- Q1.rb
+++ Q1.rb
@@ -1,6 +1,8 @@
 
 #Q1
+
 puts "\n\t #{(1+2) *3}\ nGoodbye )"
+
 ##########################
 # 9nGoodbye ) is the result in the output: the output takes 2+1=3 and multiply it by 3 again, result is 9. in Addition because of the 
 # carachters \n and \t the printout is in a new line [\n] and shifted left by tab [\t]
